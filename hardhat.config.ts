b916149ed83fcf653800128d267934c4ecc14dfd
leekt <leekt216@gmail.com>
2021-06-21 01:04:22 +0900

    tested with 0.8.5

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,13 @@ export default {
   solidity: {
     compilers :[
       {
-        version: "0.8.0",
-        //settings: {
-        //  optimizer: {
-        //    enabled: true,
-        //    runs: 200
-        //  }
-        //}
+        version: "0.8.5",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       }
     ]
   },
